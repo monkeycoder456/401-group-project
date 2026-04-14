007f0053b8ccc84c4f3c7fc0acacaa332dc0e06f
monkeycoder456 <167249735+monkeycoder456@users.noreply.github.com>
2026-04-14 16:42:38 -0400

    maze shit
    maze i am deeming complete do not touch

--- Code/Mazery.gd
+++ Code/Mazery.gd
@@ -16,7 +16,8 @@ var nooks = [] #rep with N
 var junk = [] #reperesented with question mark
 var open = [] #for enemy requesting a tile to run to
 
-
+@export var simplcity := 2
+'''the above variable is for how many times the tree cleaner will slim the maze down'''
 @export var danger_value := 10
 @export var my_seed : int
 @export var debug_switch := false
@@ -26,10 +27,15 @@ var open = [] #for enemy requesting a tile to run to
 @export var crossings_to_other := true
 @export var remove_orphans := true
 @export var attach_hallways := true
+@export var pitty_connections := true
+@export var clean_up_crossings := true
+@export var tree_clean_up := true
+@export var remake_if_unsolvable := true
 @export var num_rows := 20
 @export var num_columns := 20
 @export_enum("N", "S", "E","W") var enter_side_set: String
 @export_enum("N", "S", "E","W") var exit_side_set: String
+@export_enum("generic","woods","graveyard","house") var shape_set: String
 
 #region // liked seeds
 #10
@@ -43,8 +49,15 @@ var open = [] #for enemy requesting a tile to run to
 #where x is the columns it occupies
 #and y is the rows it occupies 
 var SHAPES = {"Square2"=[2,2], "Rectangle32"=[3,2],"Rectangle23"=[2,3],"Square3"=[3,3],"Rectangle42"=[4,2],"Rectangle24"=[2,4],"Square4"=[4,4]}
+var WOODS_SHAPES = {"Square3"=[3,3],"Rectangle34"=[3,4],"Rectangle43"=[4,3],"Square2"=[2,2],"rectangle64"=[6,4],"rectangle46"=[4,6]}
+var GRAVE_SHAPES = {"square3"=[3,3],"square4"=[4,4],"rectangle54"=[5,4],"rectangle45"=[4,5],"Rectangle61"=[6,1],"Rectangle16"=[1,6],"dittoRectangle61"=[6,1],"dittoRectangle16"=[1,6]}
+var HOUSE_SHAPES = {"Square3"=[3,3],"Square4"=[4,4],"Rectangle62"=[6,2],"Rectangle26"=[2,6],"dittoSquare3"=[3,3],"dittoSquare4"=[4,4]}
+var MAZE_SIZES = {"generic" : [40,20],"woods" : [40,30],"graveyard" : [40,20],"house" : [20,20]}
+#NOTE: woods creation pipeline; remove brother surround, remove brother corners
+#NOTE: grave creation pipeline; remve brother surround
+#NOTE: house creation pipeline; 
 
-
+#WARNING: all mazes MUST haave remove orphans and attach hallways as part of pipeline
 signal put_something_here_please
 signal spawn_enemies
 signal spawn_items
@@ -66,8 +79,27 @@ func _ready():
 	#for i in ordinal.keys().size():
 		#var dir = ordinal.get(ordinal.keys()[i])
 		#set_cell(Vector2i(dir[0],dir[1]),1,Vector2i(3,3))
-	_make_me_maze(enter_side_set,exit_side_set)
 
+
+	if remake_if_unsolvable == true:
+		
+		var solvable = false
+		while(solvable == false):
+			_make_me_maze(enter_side_set,exit_side_set,shape_set)
+		#verify if maze is solvable
+		#just create a tree from start
+		#check if the end spot is in tree
+		#if not, make an entirely new maze
+			var treeze = max_tree_from_here($MazeryExtraData.start_tile)
+			if treeze.get_all_nodes_as_list().has($MazeryExtraData.end_tile):
+				print("VALID :-)")
+				solvable = true
+			else:
+				print("INVALID :-(")
+				pass
+	else:
+		_make_me_maze(enter_side_set,exit_side_set,shape_set)
+	proxy_to_tile_map_layer()
 	#print(get_children())
 	#print(get_child(1))
 	#print($MazeryExtraData)
@@ -90,16 +122,34 @@ func _ready():
 	else:
 		#print("No player handler")
 		pass
-		
 
  #Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if debug_switch == true and Input.is_action_just_pressed("ui_focus_next"):
-		_make_me_maze(enter_side_set,exit_side_set)
+		if remake_if_unsolvable == true:
+			
+			var solvable = false
+			while(solvable == false):
+				_make_me_maze(enter_side_set,exit_side_set,shape_set)
+			#verify if maze is solvable
+			#just create a tree from start
+			#check if the end spot is in tree
+			#if not, make an entirely new maze
+				var treeze = max_tree_from_here($MazeryExtraData.start_tile)
+				if treeze.get_all_nodes_as_list().has($MazeryExtraData.end_tile):
+					print("VALID :-)")
+					solvable = true
+				else:
+					print("INVALID :-(")
+					pass
+		else:
+			_make_me_maze(enter_side_set,exit_side_set,shape_set)
+		proxy_to_tile_map_layer()
 		var end_point = $MazeryExtraData.end_tile
 		var start_point = $MazeryExtraData.start_tile
 		spawn_player.emit(start_point)
 		call_deferred("emit_signal","spawn_enemies",end_point)
+		clearing.emit()
 
 func binToInt(array : Array):
 	if array[0] == false:
@@ -130,18 +180,54 @@ func shapeToVectorPair(topLeft : Vector2i,shapeDimensions : Array):
 	return [topLeft, bottomRight]
 
 func _make_me_maze_again(enterance_side,exit_side):
-	_make_me_maze(enterance_side,exit_side)
+	if remake_if_unsolvable == true:
+		var solvable = false
+		while(solvable == false):
+			_make_me_maze(enter_side_set,exit_side_set,shape_set)
+		#verify if maze is solvable
+		#just create a tree from start
+		#check if the end spot is in tree
+		#if not, make an entirely new maze
+			var treeze = max_tree_from_here($MazeryExtraData.start_tile)
+			if treeze.get_all_nodes_as_list().has($MazeryExtraData.end_tile):
+				print("VALID :-)")
+				solvable = true
+			else:
+				print("INVALID :-(")
+				pass
+	else:
+		_make_me_maze(enter_side_set,exit_side_set,shape_set)
+	proxy_to_tile_map_layer()
 	var end_point = $MazeryExtraData.end_tile
 	var start_point = $MazeryExtraData.start_tile
 	#print(end_point)
 	spawn_player.emit(start_point)
 	call_deferred("emit_signal","spawn_enemies",end_point, danger_value)
 
-func _make_me_maze(enterance_side: String,exit_side: String):
+func _make_me_maze(enterance_side: String,exit_side: String, shapeset : String):
+	#NOTE: write an exception for the origin tile so the maze is always connected.
+	
 	#fill world with debug tile (le BLANK tile)
 	clear()
 	var rows = num_rows
 	var columns = num_columns
+	match shapeset:
+		"generic":
+			rows = num_rows
+			columns = num_columns
+		"woods":
+			rows = MAZE_SIZES.get("woods")[1]
+			columns = MAZE_SIZES.get("woods")[0]
+		"graveyard":
+			rows = MAZE_SIZES.get("graveyard")[1]
+			columns = MAZE_SIZES.get("graveyard")[0]
+		"house":
+			rows = MAZE_SIZES.get("house")[1]
+			columns = MAZE_SIZES.get("house")[0]
+		_:
+			rows = num_rows
+			columns = num_columns
+
 	proxy = tilemapproxy.new(rows,columns)
 	blocks = blocklayer.new(rows,columns)
 	var origin_tile = Vector2i(columns/2,rows/2)
@@ -164,27 +250,32 @@ func _make_me_maze(enterance_side: String,exit_side: String):
 	#print(blocks.check_if_area_free(shape_vectors[0],shape_vectors[1]))
 	#blocks.fill_area_with_num(0,shape_vectors[0],shape_vectors[1])
 	#blocks.printMe()
-	
-	block_placing(origin_tile)
+
+	block_placing(origin_tile,shapeset)
 	#print("exited block placing. here is the block map")
 	#blocks.printMe()
 	#print("converting block map into tilemap proxy...")
 	#proxy.printMe()
+
 	block_to_tile_proxy()
 	#print("done with conversion")
 	#proxy.printMe()
 	#print("pushing changes to tile map earily: WARNING THIS IS FOR DEBUGGING ONLY")
 	proxy_to_tile_map_layer()
+
 	#print("connecting sections")
 	proxy.connect_sections()
+
 	#print("pushing changes to tile map earily: WARNING THIS IS FOR DEBUGGING ONLY")
 	proxy_to_tile_map_layer()
-	if remove_brother_surrounded == true:
+
+	if shapeset == "generic" or shapeset == "woods" or shapeset == "graveyard":
 		#print("removing redundent tiles")
 		remove_redundent()
 		#print("pushing changes to tile map earily: WARNING THIS IS FOR DEBUGGING ONLY")
 		proxy_to_tile_map_layer()
-	if remove_brother_corners == true:
+
+	if shapeset == "generic" or shapeset == "woods":
 		#print("classifying tiles")
 		tile_discriminator()
 		#print("looking for bad patter: two coners next to eachother")
@@ -193,6 +284,7 @@ func _make_me_maze(enterance_side: String,exit_side: String):
 		proxy_to_tile_map_layer()
 		#print("emptying old classified tiles.")
 		dump_old_class()
+	print("IN STEP 2")
 	if remove_hallway_surrounded == true:
 		#print("classifying tiles")
 		tile_discriminator()
@@ -211,20 +303,132 @@ func _make_me_maze(enterance_side: String,exit_side: String):
 		dump_old_class()
 		#print("pushing changes to tile map earily: WARNING THIS IS FOR DEBUGGING ONLY")
 		proxy_to_tile_map_layer()
-	if remove_orphans == true:
-		var start_tile
-		var setted = false
+
+	print("FINISHED FILTERS")
+	#region FORCE CONNECTION TO CENTER IF POSSIBLE
+	#here would go the exception for the origin tile. force it in manually.
+	put_something_here_please.emit(origin_tile, Vector2i(5,2))
+	#get all open neighbors to the center tile
+	#connect them to center tile
+	#make the center tile the appropriate tile 
+	
+	for dir in DIRECTIONS:
+		#print(dir)
+		var neighbor = origin_tile + DIRECTIONS.get(dir)
+		if proxy.getpoint(neighbor).has(true):
+			#this is a carved path horay
+			#connect it to the center point
+			proxy.carvePath(origin_tile,dir)
+	#endregion
+	
+	#remove orphans it to harsh. this can remove basically the whole
+	#fucking maze. we can work around this by forcing connections
+	#between adjecent cells that are NOT connected.
+	if pitty_connections == true:
+		pass
+		#for every tile in the proxy
+		#grab it's neighbors
+		#check if that nieghbor is CARVED but NOT CONNECTED
+		#force a connection anyways.
+		var delta_queue = []
 		for row in proxy.myMatrix.size():
 			for slot in proxy.myMatrix[0].size():
+				#var data = proxy.getpoint(Vector2i(slot,row))
+				for dir in DIRECTIONS:
+					#await get_tree().create_timer(1).timeout
+					var neighbor = Vector2i(slot,row) + DIRECTIONS.get(dir)
+					if inbounds(neighbor) and proxy.carved(Vector2i(slot,row)):
+						var neighborData = proxy.getpoint(neighbor)
+						if proxy.carved(neighbor) and !proxy.connected_to_cell(Vector2i(slot,row),dir):
+							#proxy.carvePath(Vector2i(slot,row),dir)
+							delta_queue.append([Vector2i(slot,row),dir])
+		for entry in delta_queue:
+			proxy.carvePath(entry[0],entry[1])
+	if remove_orphans == true:
+		var start_tiles = []
+		var made = false
+		#this gets top right
+		for row in proxy.myMatrix.size() - 1:
+			for slot in proxy.myMatrix[0].size()-1:
 				if proxy.carved(Vector2i(slot,row)):
-					start_tile = Vector2i(slot,row)
-					setted = true
+					start_tiles.append(Vector2i(slot,row))
+					made = true
 					break
-			if setted == true:
+			if made == true:
 				break
-		killer_orphans(start_tile)
+		#get top left
+		made = false
+		for row in proxy.myMatrix.size() -1 :
+			for slot in range(proxy.myMatrix[0].size()-1,-1,-1):
+				if proxy.carved(Vector2i(slot,row)):
+					start_tiles.append(Vector2i(slot,row))
+					made = true
+					break
+			if made == true:
+				break
+		#get get bottom right
+		made = false
+		for row in range(proxy.myMatrix.size()-1,-1,-1):
+			for slot in proxy.myMatrix[0].size()-1:
+				if proxy.carved(Vector2i(slot,row)):
+					start_tiles.append(Vector2i(slot,row))
+					made = true
+					break
+			if made == true:
+				break
+		#get the bottom left
+		made = false
+		for row in range(proxy.myMatrix.size()-1,-1,-1):
+			for slot in range(proxy.myMatrix[0].size()-1,-1,-1):
+				if proxy.carved(Vector2i(slot,row)):
+					start_tiles.append(Vector2i(slot,row))
+					made = true
+					break
+			if made == true:
+				break
+		#test all 4 corners, compare trees
+		#get the bigest tree
+		#use that one for elimination
+		
+		#make 4 trees with each coord pair
+		var trees = []
+		for pair in start_tiles:
+			set_cell(pair,0,Vector2i(8,9))
+			trees.append(await max_tree_from_here(pair))
+		
+		#for tree in trees:
+			#print(tree.get_all_nodes_as_list().size())
+		
+		#remove small trees, this should leave us with one biggest tree
+		var biggest_tree
+		var biggest_num = -1
+		for tree in trees:
+			if tree.get_all_nodes_as_list().size() > biggest_num:
+				biggest_tree = tree
+				biggest_num = tree.get_all_nodes_as_list().size()
+		#print("biggest tree", biggest_tree.get_all_nodes_as_list().size())
+		
+		killer_orphans(biggest_tree)
+		
 		#print("pushing changes to tile map earily: WARNING THIS IS FOR DEBUGGING ONLY")
+#region debug comment out
 		proxy_to_tile_map_layer()
+	if clean_up_crossings == true:
+		#the pitty connection step added way to many X-ings
+		#get every crossing, preform a flood fill beginning a crossing
+		#which is ENTIRELY SURROUNDED BY CARVED CELLS (INCLUDING DIAGONALS)
+		#the flood fill should find other crossing who also have the same 
+		#condition.
+		#tile_discriminator()
+		
+		for cross in crossing:
+			pass
+		#once the flood fill is complete, add all the cells in the flood fill
+		#to an array to mark their deletion.
+		
+		#then uncarve every cell found in the array
+		
+		pass
 	if attach_hallways == true:
 		#print("emptying old classified tiles, also there is no need to classify anymore")
 		dump_old_class()
@@ -249,35 +453,118 @@ func _make_me_maze(enterance_side: String,exit_side: String):
 	var exit_node = get_node("exit_zone")
 	print("exit_node: ", exit_node)
 	exit_node.global_position = map_to_local($MazeryExtraData.end_tile)
+	
+	if tree_clean_up == true:
+	#create a tree, find every leaf
+	#remove every leaf (BUT THE EXIT)
+		var iteration_count = 0
+		var start_tile = $MazeryExtraData.start_tile
+		var untouched_maze_tree = await max_tree_from_here_but_directed_tho(start_tile)
+		var maze_tree = await max_tree_from_here_but_directed_tho(start_tile)
+		print(maze_tree.my_graph)
+		print("I have this many leaves: ",maze_tree.get_all_leaves())
+		for leaf in maze_tree.get_all_leaves():
+			set_cell(leaf,2,Vector2i(1,0))
+		while(maze_tree.get_all_leaves().size() > 1 and iteration_count <simplcity):
+			#the reason for 1 is that we need the exit leaf, it is always a leaf
+			#print("num leaves: ",maze_tree.get_all_leaves().size())
+			var all_leaves = maze_tree.get_all_leaves()
+			for leaf in maze_tree.get_all_leaves():
+				set_cell(leaf,2,Vector2i(3,0))
+				await get_tree().create_timer(0.001).timeout
+			for leaf in all_leaves:
+				if leaf != $MazeryExtraData.end_tile:
+					maze_tree.prune(leaf)
+			iteration_count += 1
+		for node in maze_tree.get_all_nodes_as_list():
+			var num_connections = maze_tree.fetch_arb_node(node).size()
+			match num_connections:
+				1:
+					set_cell(node,2,Vector2i(0,1))
+				2:
+					set_cell(node,2,Vector2i(1,1))
+				3:
+					set_cell(node,2,Vector2i(2,1))
+				4:
+					set_cell(node,2,Vector2i(3,1))
+				_:
+					print("this node thinks he is special")
+					print(node)
+					print(maze_tree.fetch_arb_node(node))
+					print(num_connections)
+			pass
+		#take the difference between untouched and touch
+		var difference = []
+		var untouched_tree_nodes = untouched_maze_tree.get_all_nodes_as_list()
+		var touched_tree_nodes = maze_tree.get_all_nodes_as_list()
+		#for every element in the larger array, check if it is NOT in smaller array
+		#if it isn't, put that node in difference.
+		for node in untouched_tree_nodes:
+			if !touched_tree_nodes.has(node):
+				difference.append(node)
+		#with the difference, uncarve all tiles in difference
+		for node in difference:
+			proxy.uncarve_all(node)
+#endregion
 
-func killer_orphans(start):
+func max_tree_from_here(start):
 	var killer_tree = Tree_graph.new(start)
 	#begin to do a depth first search
 	var visited = []
 	var new = killer_tree.get_all_nodes_as_list()
-	var accumulator = 0
 	#print("working")
 	while !new.is_empty():
 		var node = new.pop_front()
 		visited.append(node)
+		set_cell(node,2,Vector2i(0,0))
+		#await get_tree().create_timer(0.1).timeout
 		for dir in DIRECTIONS:
 			var neighbor = node + DIRECTIONS.get(dir)
-			if !visited.has(neighbor) and inbounds(neighbor) and (proxy.connected_to_cell(node,dir)) and !new.has(neighbor):
+			#has to allow connection to already in tree nodes.
+			#the only distinction is that the asked neighbor must NOT be in the node's neighbor list already
+			if (!visited.has(neighbor) and inbounds(neighbor) and (proxy.connected_to_cell(node,dir)) and !new.has(neighbor)) or (node == $MazeryExtraData.start_tile and !new.has(neighbor) and !visited.has(neighbor) and inbounds(neighbor)) or (neighbor == $MazeryExtraData.end_tile):
 				#its new
 				new.append(neighbor)
 				killer_tree.add_node(node,neighbor)
-		#print(visited.size())
-		#print(new.size())
-		accumulator += 1
-	#print(killer_tree.get_all_nodes_as_list().size())
-	#print_rich("[color=red]acummulator value: [/color]", accumulator)
-	#print("varifying if the tree is correect")
+			#elif((visited.has(neighbor) and inbounds(neighbor) and inbounds(node) and proxy.connected_to_cell(node,dir)) and !killer_tree.fetch_arb_node(neighbor).has(node)):
+				#killer_tree.add_node(node,neighbor)
+				#set_cell(neighbor,2,Vector2i(2,0))
+		#await get_tree().create_timer(0.01).timeout
+	return killer_tree
+
+func max_tree_from_here_but_directed_tho(start):
+	var killer_tree = Directed_tree.new(start)
+	#begin to do a depth first search
+	var visited = []
+	var new = killer_tree.get_all_nodes_as_list()
+	#print("working")
+	while !new.is_empty():
+		var node = new.pop_front()
+		visited.append(node)
+		set_cell(node,2,Vector2i(0,0))
+		for dir in DIRECTIONS:
+			var neighbor = node + DIRECTIONS.get(dir)
+			#has to allow connection to already in tree nodes.
+			#the only distinction is that the asked neighbor must NOT be in the node's neighbor list already
+			if (!visited.has(neighbor) and inbounds(neighbor) and (proxy.connected_to_cell(node,dir)) and !new.has(neighbor)) or (node == $MazeryExtraData.start_tile and !new.has(neighbor) and !visited.has(neighbor) and inbounds(neighbor)) or (neighbor == $MazeryExtraData.end_tile):
+				#its new
+				new.append(neighbor)
+				killer_tree.add_node(node,neighbor)
+			#elif(visited.has(neighbor) and inbounds(neighbor) and inbounds(node) and proxy.connected_to_cell(node,dir) and !killer_tree.fetch_arb_node(neighbor).has(node)):
+				#killer_tree.add_node(node,neighbor)
+				#set_cell(node,2,Vector2i(2,0))
+		#await get_tree().create_timer(0.01).timeout
+	return killer_tree
+
+func killer_orphans(killer_tree):
 	for pair in killer_tree.get_all_nodes_as_list():
-		set_cell(pair,1,Vector2i(0,7))
+		set_cell(pair,0,Vector2i(0,9))
 	for row in proxy.myMatrix.size():
 		for slot in proxy.myMatrix[0].size():
 			if !killer_tree.get_all_nodes_as_list().has(Vector2i(slot,row)):
-				proxy.uncarve_all(Vector2i(slot,row))
+				if get_cell_tile_data(Vector2i(slot,row)).get_custom_data("N") or get_cell_tile_data(Vector2i(slot,row)).get_custom_data("S") or get_cell_tile_data(Vector2i(slot,row)).get_custom_data("E") or get_cell_tile_data(Vector2i(slot,row)).get_custom_data("W"):
+					proxy.uncarve_all(Vector2i(slot,row))
+					set_cell(Vector2i(slot,row),0,Vector2i(8,9))
 
 func find_valid_offset(edge : Vector2i,shapedefinition : Array):
 	var offset = Vector2i(0,0)
@@ -300,19 +587,35 @@ func find_valid_offset(edge : Vector2i,shapedefinition : Array):
 			#if abs(offset.x) > shapedefinition.x then set offset.x to 0; decrement offset.y by -1
 			#if abs(offset.y) > shapedefintion.y then return vector2(-inf,-inf) AKA didnt work
 
-func block_placing(originVect : Vector2i):
+func block_placing(originVect : Vector2i,shapeset):
+	print("INSIDE BLOCK_PLACING")
 	#origin vector is where the "holy square will go"
 	blocks.writepoint(originVect,0)
 	var can_place = true
 	var block_num = 1
+	var used_shape_set : Dictionary
+	match shapeset:
+		"generic":
+			used_shape_set = SHAPES
+		"woods":
+			used_shape_set = WOODS_SHAPES
+		"graveyard":
+			used_shape_set = GRAVE_SHAPES
+		"house":
+			used_shape_set = HOUSE_SHAPES
+		_:
+			used_shape_set = SHAPES
+	print("using shape set: ",shapeset)
 	while(can_place):
+			#await get_tree().create_timer(1).timeout
+			print("I AM STUCK")
 		#print("should we be working?", can_place)
 		#get a random shape
 		#region shape selected. tryign to use it
 		#instead shuffle the shapes array and do a for loop
 		#when encountering an unusable shape, skip it
 		#when we run out of shapes. shuffle and repeat
-			var shapes_to_use = SHAPES.values()
+			var shapes_to_use = used_shape_set.values()
 			shapes_to_use.shuffle()
 			for shape_to_place in shapes_to_use:
 				var shape_vectors = shapeToVectorPair(Vector2i(0,0),shape_to_place)
--- Code/tree.gd
+++ Code/tree.gd
@@ -1,9 +1,12 @@
 class_name Tree_graph extends Object
 
 var my_graph : Dictionary
+var my_root
+
 
 func _init(root):
 	my_graph[root] = [] 
+	my_root = root
 
 func add_node(parent_node,new_child_node):
 	#var children = my_graph.get(parent_node)
@@ -15,3 +18,72 @@ func add_node(parent_node,new_child_node):
 
 func get_all_nodes_as_list():
 	return my_graph.keys()
+
+
+func get_all_leaves():
+	var leaves = []
+	for key in my_graph.keys():
+		#print(my_graph.get(key).size())
+		#print(my_graph.get(key).size() == 0)
+		if my_graph.get(key).size() == 1:
+			print("leaf")
+			leaves.append(key)
+	print("here are the leaves I reported: ", leaves)
+	return leaves
+
+func fetch_arb_node(node):
+	'''gets an arbitrary node's children'''
+	return my_graph.get(node)
+
+func get_branch(root):
+	'''quickly fetches all the children of that node and hands over a tree\n
+	as if that node is the root'''
+	var branch = Tree_graph.new(root)
+	#var children_of_new_root = my_graph.get(root)
+	#for child in children_of_new_root:
+		#branch.add_node(root, child)
+
+	var new_children_added = true
+	var visited = []
+	while(new_children_added):
+		new_children_added = false
+		for leaf in branch.get_all_leaves():
+			if visited.has(leaf):
+				continue
+			else:
+				visited.append(leaf)
+			var new_kids = my_graph.get(leaf,0)
+			if new_kids != 0:
+				new_children_added = true
+			for kid in new_kids:
+				branch.add_node(leaf,kid)
+	
+	return branch
+
+func prune(node):
+	'''this will remove the node and all nodes after it.'''
+	if my_graph.get(node).size() == 0:
+		#case 1, the pruned thing is a leaf. remove it
+		#	and connects all parents would have
+		my_graph.erase(node)
+		#iterate through the whole tree to remove
+		#parent's connection.
+		var nodes = get_all_nodes_as_list()
+		for noode in nodes:
+			var children_of_node = my_graph.get(noode)
+			children_of_node.erase(node)
+	else:
+		#pruned thing has kids
+		#remove pruned thing, and all of it's decendants, then do the 
+		#steps in part one
+		var branch_to_prune = get_branch(node)
+		for noode in branch_to_prune.get_all_nodes_as_list():
+			my_graph.erase(node)
+		
+		#the branch we pruned may have connections elsewere in greater tree.
+		#fix that.
+		var nodes = get_all_nodes_as_list()
+		for noode in nodes:
+			var children_of_node = my_graph.get(noode)
+			for bode in branch_to_prune.get_all_nodes_as_list():
+				children_of_node.erase(bode)
