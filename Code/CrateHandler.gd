584e2dc679a970c03cba42ffbd4c612cc538abc2
KwabOwus <157089584+KwabOwus@users.noreply.github.com>
2026-04-17 01:37:18 -0400

    Updated Crate & Handler
    Currently an issue where the crate does not spawn into the tilemap, still attempting to fix but sending this here until I get a fix

--- Code/CrateHandler.gd
+++ Code/CrateHandler.gd
@@ -1,56 +1,74 @@
 class_name CrateHandler extends Sprite2D
 #Exists to span crates at the beginnig of a level
 
-var item_spawn_point := Vector2i(-1,-1)
 var player_reference : Node
-var the_maze : Node
+var the_maze : TileMapLayer
 var an_item : Node
 
+
 signal spawn_items
-
-@export var crate_path = preload("res://scenes/crate.tscn")
 @export var coin_path = preload("res://scenes/coin.tscn")
-
+@export var crate_path = preload("res://scenes/crate.tscn")
 
 #The item pool, the crate will pick an item found here
 var item_pool = {"coin":coin_path}
+#var chosen_item = item_pool.keys().pick_random()
 
 func _ready():
 	set_modulate(Color(0.581, 0.454, 0.0, 1.0))
 	if get_parent() is Mazery:
 		print("Wake up item")
+		
+		the_maze = get_parent()
+
 		get_parent().spawn_items.connect(_spawnCrates)
 		get_parent().clearing.connect(_DestroyCrate)
 		
 	else:
 		print("Not the parent")
 	
+func get_open_cells():
+	var all_cells = the_maze.get_used_cells()
+	var all_open = all_cells.filter(used_filter)
+	
+	return all_open
+	
+func used_filter(cell): 
+	if the_maze.get_cell_tile_data(cell).get_custom_data("N") or the_maze.get_cell_tile_data(cell).get_custom_data("S") or the_maze.get_cell_tile_data(cell).get_custom_data("E") or the_maze.get_cell_tile_data(cell).get_custom_data("W"):
+		return true
+		
+	else:
+		return false
 
-
-func _spawnCrates(location : Vector2i, danger_value : int):
+func _spawnCrates(location, danger_value : int):
 #Function should spawn a specific amount of crates
 #The # of crates spawned would be in range of the danger value
 	print("Spawning Crate!")
-	item_spawn_point = location
+
 	
 	for unit in range(danger_value):
-		#insanciate a crate and add to group crate
-		var crate = crate_path.insanciate().add_to_group("crate") 
-		the_maze = get_parent()  #Maze Reference
-		print("I got the maze reference")
-		set_modulate(Color(0.21, 0.119, 0.33, 1.0))
-		pass
+		var crate = crate_path.instantiate()
+		crate.add_to_group("crate")
+		get_parent( ).add_child(crate)
+		
+		location = get_open_cells().pick_random()
+		crate.myItem = _pickItem()
+		
+		crate.position = the_maze.map_to_local(location)  
+		
+	print("I got the maze reference")
+	
+	set_modulate(Color(0.21, 0.119, 0.33, 1.0))
 
-#void add_to_group(group: StringName, persistent: bool = false)
-#To add to group 
 
-	pass
+
+func _pickItem():
+		return item_pool.get(item_pool.keys().pick_random())
 
 func _DestroyCrate():
 	print("Clearing crate")
-	get_tree().call_group("crate", "forcedestroy")
+	
+	get_tree().call_group("crate", "_force_destroy")
 	
 	set_modulate(Color(0.237, 0.518, 0.187, 1.0))
-	pass
-	
-	
+	queue_free()
--- Code/crate.gd
+++ Code/crate.gd
@@ -1,34 +1,78 @@
-class_name crate extends Node2D
+class_name Crate extends GenericMazeEntity
+#Drops loot for the player to use against the swarm of enemies
 
-
+signal points
+var point_total = 100
+var myItem : PackedScene 
 var state = "notBroken"
 var player_in_area = false
 
-var coin = preload("res://scenes/coin.tscn")
 
-func _process(delta):
-	if state == "Not Broken":
-		$AnimatedSprite2D.play("Not Broken")
-	if state == "Broken":
-		$AnimatedSprite2D.play("Broken")
-		if player_in_area:
-			if Input.is_action_just_pressed("e"):
-				state = "Broken"
-				
-
-func _on_breakable_area_body_entered(body) :
-	for group in get_groups():
-		if not str(group).begins_with("player"):
-			player_in_area = true
-			
-
-func _on_breakable_area_body_exited(body) :
-	for group in get_groups():
-		if not str(group).begins_with("player"):
-			player_in_area = false
+func _ready():
+	print("Crate in ready func")
+	print("My current_cell is: ", current_cell)
+	
+	#current_cell = await get_parent().spawn_items
+	#target_position = tilemap.map_to_local(current_cell)
+	#global_position = target_position
+	
+	#print("My current_cell is: ", current_cell)
+	
+	print("Crate has finished waiting")
 	
 
-func drop_coin():
-	var coin_instance = coin.instanciate()
-	coin_instance.global_position = $Marker2D.global_position
-	get_parent().add_child(coin_instance)
+func _process(delta):
+	#if state == "Not Broken":
+	#	$AnimatedSprite2D.play("Not Broken")
+	#if state == "Broken":
+	#	$AnimatedSprite2D.play("Broken")
+	#	if player_in_area:
+	#		 #On contact instead
+	#		state = "Broken"
+	pass
+
+func _who_touched_me(node: Node2D):
+	if node.is_in_group("player"):
+		print("Player has touched a crate, destroying...")
+		_destroy()
+		
+
+func _destroy():
+	$AnimatedSprite2D.play("Broken")
+	if myItem:
+		var currentItem = myItem.instantiate()
+		currentItem.global_position = global_position
+		get_parent().add_child(currentItem)
+	else:
+		print("Warning: myItem is null")
+	
+	await $AnimatedSprite2D.animation_finished 
+	queue_free()
+	
+func _force_destroy():
+	$AnimatedSprite2D.play("Broken")
+	await $AnimatedSprite2D.animation_finished 
+	queue_free()
+	
+	
+
+#func _on_area_2d_body_entered(body: Node2D) -> void:
+#	if body.is_in_group("player"):
+#		print("player touch crate")
+#		pass
+
+#func _on_breakable_area_body_entered(body) :
+#	for group in get_groups():
+#		if not str(group).begins_with("player"):
+#			player_in_area = true
+
+#func _on_breakable_area_body_exited(body) :
+#	for group in get_groups():
+#		if not str(group).begins_with("player"):
+#			player_in_area = false
+
+#func drop_coin():
+#	var coin_instance = coin.instanciate()
+#	coin_instance.global_position = $Marker2D.global_position
+#	get_parent().add_child(coin_instance)
+	
